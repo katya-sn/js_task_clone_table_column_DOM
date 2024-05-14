abfd7a97d7649f7887a209356117da6dd1e5e244
katya-sn <kattya.sagun@gmail.com>
2024-05-14 13:40:05 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,24 @@
+"use strict";
+const tbody = document.querySelector("tbody");
+const thead = document.querySelector("thead");
+const tfoot = document.querySelector("tfoot");
+const tbodyRows = [
+    ...tbody.rows
+];
+const headerCell = thead.rows[0].cells[1].cloneNode(true);
+thead.rows[0].insertBefore(headerCell, thead.rows[0].lastElementChild);
+const footerCell = tfoot.rows[0].cells[1].cloneNode(true);
+tfoot.rows[0].insertBefore(footerCell, tfoot.rows[0].lastElementChild);
+const copy = [];
+tbodyRows.forEach((row)=>{
+    const cellDuplicate = row.cells[1];
+    copy.push(cellDuplicate);
+});
+tbodyRows.forEach((tr)=>{
+    const newCell = tr.insertCell(4);
+    const itemIndex = tr.sectionRowIndex;
+    const content = copy[itemIndex].textContent;
+    newCell.textContent = content;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
